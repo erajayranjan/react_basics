f9f27f40df5821fafb5feb395493d2420cd95b54
erajayranjan <ajayranjan@live.com>
2021-02-21 15:44:28 +0530

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import "../style.css";
 export default function Header(){
   return (
       <div className="w-full" >
-       
+       This is a Header loThis is a Footer Section Demo!
+This is a Footer Section Demo!
+This is a Footer Section Demo!
       </div>
   );
 }
