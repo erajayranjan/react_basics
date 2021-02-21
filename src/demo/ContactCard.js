ac5fa5d82680a5dda60c0dbe6f567571746ae05f
erajayranjan <ajayranjan@live.com>
2021-02-21 15:38:05 +0530

    1 file modified

--- src/demo/ContactCard.js
+++ src/demo/ContactCard.js
@@ -5,7 +5,7 @@ import "../style.css";
 
 export default function ContactCard(props){
   return (
-    <div className="flex flex-col justify-center font-bold content-center text-sm m-1 p-1 sm: w-5/12 md: w-1/4 text-center bg-blue-100 text-blue-900 shadow-2xl">
+    <div className="flex flex-col justify-center font-bold content-center text-sm m-1 p-1 sm: w-5/12 md: w-1/5 text-center bg-blue-100 text-blue-900 shadow-2xl">
       <img src={props.contact.img} alt="image"
 
       />
