3b0dad2f255e556b8545370956794316bd565a6d
erajayranjan <ajayranjan@live.com>
2021-02-28 01:39:46 +0530

    2 files modified

--- src/components/About.js
+++ src/components/About.js
@@ -68,14 +68,13 @@ export default function About(){
 
 					</section>
 
-				 //Footer
+
 					<footer id="footer">
 						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
 					</footer>
 
 			</div>
 
-		// Scripts
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/jquery.poptrox.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
