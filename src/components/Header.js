b4c017c1a9fe1b74ce93592bac330e5c3d7f1dbe
erajayranjan <ajayranjan@live.com>
2021-02-28 00:31:49 +0530

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,32 @@ import React from "react";
 import "../style.css";
 export default function Header(){
   return (
-      <div className="w-full" >
-       This is a Header loThis is a Footer Section Demo!
-This is a Footer Section Demo!
-This is a Footer Section Demo!
+      <div className="w-full bg-blue-800 p-4 " >
+        <nav className="flex justify-between">
+          <div>
+            <img    
+              src="https://lh3.googleusercontent.com/ogw/ADGmqu9nXSPJq6SgQ8Rcs5ApqYORU9WgfWZTp1bvFDFB7w=s32-c-mo" alt="logo" 
+            />
+          </div>
+          <div>
+            <ul>
+              <li>
+                Home
+              </li>
+              <li>
+                About
+              </li>
+              <li>
+                Contact
+              </li>
+            </ul>
+          </div>
+          <div>
+              <img    
+              src="https://lh3.googleusercontent.com/ogw/ADGmqu9nXSPJq6SgQ8Rcs5ApqYORU9WgfWZTp1bvFDFB7w=s32-c-mo" alt="logo" 
+            />
+          </div>
+        </nav>
       </div>
   );
 }
