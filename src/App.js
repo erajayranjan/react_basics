4ee54de65e89616ab8cf0a617827d7bd81b6961d
erajayranjan <ajayranjan@live.com>
2021-03-06 19:57:20 +0530

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from "./components/About";
 
 export default function App() {
   return (
-    <div className="" >
+    <div  >
       <Header />
       <Main />
       <about />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../style.css";
-import "./data/Profile";
+import Profile from ".components/data/Profile";
 
 export default function Header(){
+   console.log(Details);
   return (
     <div className="flex items-center justify-center bg-gradient-to-tl from-gray-900 to-gray-300  p-5 w-full h-24 text-gray-100" style={{ backgroundColor: "", padding:"", color:"" }}>
         <h1 > 
-          &#xa9; trainU Academy { Details.id  }
+          &#xa9; trainU Academy 
         </h1>
         
         <p>
